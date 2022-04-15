a9af03f84573400b6d62afa9c814da131a19c67e
IqbalNugraha <fniqbal4@gmail.com>
2022-04-15 16:59:01 +0700

    latihan akb

---
+++ app/src/main/java/com/example/a10119199latihan1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.a10119199latihan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+//14 April 2022, 10119199, Iqbal Fatur Nugraha, IF5
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
